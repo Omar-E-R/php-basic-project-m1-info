1c91a715bf309cee4e84ebe547df3035768db8bd
Omar EL RIFAI <omar.elrifai@qemotion.com>
2022-03-23 19:47:43 +0100

    Add ReadDirRec.php that reads recursevily a dir

---
+++ ReadDirRec.php
@@ -0,0 +1,83 @@
+
+<P>
+<B>DEBUTTTTTT DU PROCESSUS :</B>
+<BR>
+<?php echo " ", date ("h:i:s"); ?>
+</P>
+<?php
+
+// Limits the maximum execution time to 500sec
+set_time_limit (500);
+$path= "docs";
+
+echo "Directory: " . $path . "<br>" . "<br>";
+
+//Execute the function on docs folder
+explorerDir($path);
+
+function explorerDir($path)
+{
+	// Open current folder and handle it to $folder
+	$folder = opendir($path);
+
+	// Looping on all files in current $folder and saving them as entries
+	while($entree = readdir($folder))
+	{
+		// Execlude "." and ".." folders from loop
+		if($entree != "." && $entree != "..")
+		{
+			// In case current entry is a directory
+			if(is_dir($path."/".$entree))
+			{
+				// Save current path in sav_path
+				$sav_path = $path;
+				// Append the current entry (directory) to current path
+				$path .= "/".$entree;
+				// Execute recursively the function on the new directory
+				explorerDir($path);
+
+				echo "<br>" . "Directory: " . $path . "<br>" . "<br>";
+
+				// Save the current path back in $path
+				$path = $sav_path;
+
+
+			}
+			else
+			{
+				// if entry is a file we append the entry to path in $path_source
+				$path_source = $path."/".$entree;
+
+				$image_type = filetype($path_source);
+				$image_size = filesize($path_source);
+				//Connecting to MySQL
+				$conn = mysqli_connect('localhost', 'root',
+					'17082015'
+				);
+				//Check if connection is good
+				if (!$conn) {
+					die("Connection failed" . mysqli_connect_error());
+				} else {
+					//Selecting 'pagination' database
+					mysqli_select_db($conn, 'pagination');
+				}
+
+				$query = "INSERT INTO `image` (`origin`, `type`, `size`, `name`)
+				VALUES ('$entree', '$image_type', '$image_size', '$path_source');";
+
+			if (mysqli_query($conn, $query)) {
+				echo "File Recorded: ". $entree . "<br>";
+			} else {
+				echo "Error: " . $query . "<br>";
+				}
+			}
+		}
+	}
+	closedir($folder);
+}
+?>
+<P>
+<B>FINNNNNN DU PROCESSUS :</B>
+<BR>
+<?php echo " ", date ("h:i:s"); ?>
+</P>
